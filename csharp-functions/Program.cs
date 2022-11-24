76179623c2abdf6eade7f0720a3c97560d7ffaa6
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-24 15:23:52 +0100

    Add project files.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Scrivete nel vostro programma principale Program.cs le seguenti funzioni di base:
+ * 
+ *  [] void StampaArrayInteri(int[] array): che preso un array di numeri interi, stampa a video il contenuto
+ *  dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. Potete prendere quella fatta in classe questa mattina
+ *  
+ *  [] int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+ *  
+ *  [] int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array
+ *  con tutti gli elementi elevati quadrato.
+ *  
+ *  [] int sommaElementiArray(int[] array): che preso un array di numeri interi,
+ *  restituisca la somma totale di tutti gli elementi dell’array.
+ *
+ *
+ *
+ * Una volta completate queste funzioni di utilità di base, e dato
+ * il seguente array di numeri [2, 6, 7, 5, 3, 9] già dichiarato nel vostro codice, si vogliono utilizzare le funzioni per:
+ * 
+ *  [] Stampare l’array di numeri fornito a video
+ *  
+ *  [] Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato
+ *  (Verificare che l’array originale non sia stato modificato quindi ristampare nuovamente l’array originale
+ *  e verificare che sia rimasto [2, 6, 7, 5, 3, 9])
+ *  
+ *  [] Stampare la somma di tutti i numeri
+ *  
+ *  [] Stampare la somma di tutti i numeri elevati al quadrati
+ */
